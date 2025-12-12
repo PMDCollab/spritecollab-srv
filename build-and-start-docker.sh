c19c52a90b6ae35c23a386d7ef82d027b4588093
Capypara <hello@capypara.de>
2025-12-12 22:25:17 +0100

    Update all dependencies (2025-12)

--- build-and-start-docker.sh
+++ build-and-start-docker.sh
@@ -5,6 +5,5 @@ docker-compose down
 mkdir workdir || true
 chmod a+rwX workdir
 cargo clippy
-cargo clippy --features discord
 docker buildx build . -t spritecollab-srv
 docker-compose up --no-log-prefix
